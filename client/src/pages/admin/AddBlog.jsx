8a11c5a522181b4dae8f37718705b61acbbf3d66
Shruti Upadhyay <shrutiupadhyay1708@gmail.com>
2025-08-24 19:14:38 +0530

    Project Completed

--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -1,33 +1,76 @@
 import React, { useEffect, useRef, useState } from "react";
 import { assets, blogCategories } from "../../assets/assets";
-import Quill from "quill"
+import Quill from "quill";
+import { useAppContext } from "../../context/AppContext";
+import toast from "react-hot-toast";
+import {parse} from "marked";
 
 const AddBlog = () => {
-
+    const {axios} = useAppContext()
+    const [isAdding, setIsAdding] = useState(false)
+    const [loading, setLoading] = useState(false)
     const editorRef =useRef(null)
     const quillRef =useRef(null)
-
     const [image, setImage] = useState(false);
     const [title, setTitle] = useState("");
     const [subTitle, setSubTitle] = useState("");
-    const [caegory, setCategory] = useState("Startup");
+    const [category, setCategory] = useState("Startup");
     const [isPublished, setIsPublished] = useState(false);
 
-    const onSubmitHandler = async (e) =>{
-        e.preventDefault();
-    }
-
     const GenerateContent = async () => {
-
+        if(!title) return toast.error("Please enter a title")
+        
+        try {
+            setLoading(true);
+            const {data} = await axios.post('/api/blog/generate', {prompt: title})
+            if(data.success){
+                quillRef.current.root.innerHTML = parse(data.content);
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }finally{
+            setLoading(false);
+        }
     }
 
+    const onSubmitHandler = async (e) =>{
+        try {
+            e.preventDefault();
+            setIsAdding(true);
+            const blog = {
+                title, subTitle, 
+                description: quillRef.current.root.innerHTML,
+                category, isPublished
+            };
+            const formData = new FormData(); 
+            formData.append('blog', JSON.stringify(blog))
+            formData.append('image', image)
+
+            const { data } = await axios.post('/api/blog/add', formData);
+            if(data.success){
+                toast.success(data.message);
+                setImage(false);
+                setTitle("");
+                quillRef.current.root.innerHTML = "";
+                setCategory("Startup");
+            }else{
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error(error.message);
+        } finally{
+            setIsAdding(false);
+        }
+    };
+    
     useEffect(() => {
         // Initialize Quill only once
         if (!quillRef.current && editorRef.current) {
             quillRef.current = new Quill(editorRef.current, {theme: 'snow'});
         }
     },[])
-
 
     return (
         <form onSubmit={onSubmitHandler} className="flex-1 bg-blue-50/50 text-gray-600 h-full overflow-scroll">
@@ -52,7 +95,13 @@ const AddBlog = () => {
                 <p className="mt-4">Blog Description</p>
                 <div className="max-w-lg h-74 pb-16 sm:pb-10 pt-2 relative">
                     <div ref={editorRef}></div>
-                    <button type="button" onClick={GenerateContent}
+                    {loading && (
+                        <div className="absolute right-0 top-0 bottom-0 left-0 
+                        flex items-center justify-center bg-black/10 mt-2">
+                            <div className="w-8 h-8 rounded-full border-2 border-t-white
+                            animate-spin"></div>
+                        </div>)}
+                    <button disabled={loading} type="button" onClick={GenerateContent}
                     className="absolute bottom-1 right-2 ml-2 text-xs text-white bg-black/70
                     px-4 py-1.5 rounded hover:underline cursor-pointer">
                         Generate with AI</button>
@@ -75,8 +124,10 @@ const AddBlog = () => {
                     cursor-pointer" onChange={e => setIsPublished(e.target.checked)}/>
                 </div>
 
-                <button type="submit" className="mt-8 w-40 h-10
-                bg-primary text-white rounded cursor-pointer text-sm">Add Blog</button>
+                <button disabled={isAdding} type="submit" className="mt-8 w-40 h-10
+                bg-primary text-white rounded cursor-pointer text-sm">
+                    {isAdding ? "Adding..." : "Add Blog"}
+                    </button>
 
             </div>
 
---
+++ server/middleware/auth.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+
+const auth = (req, res, next)=>{
+    const authHeader = req.headers.authorization;
+    if(!authHeader) return res.json({success: false, message: "No token provided"});
+    const token = authHeader.split(" ")[1]; // get the token after 'Bearer'
+    try{
+        jwt.verify(token, process.env.JWT_SECRET)
+        next()
+    } catch (error){
+        res.json({success: false, message: "Invalid token"})
+    }
+}
+
+export default auth;
